d7ccce05f409b5384ee93cb0e82fac5133dac429
bclausdorff <b.clausdorff@posteo.de>
2016-10-11 22:29:12 +0200

    Query for head rotation

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ if (env and env.startswith('Google App Engine/')):
   # Connecting from App Engine
   db = MySQLdb.connect(
     unix_socket=env_config['MYSQL_SOCKET_PATH'],
-    user=env_config['MYSQL_USER'])
+    user=env_config['MYSQL_USER'],
+    passwd=env_config['MYSQL_PASSWORD'])
 else:
   # Connecting from an external network.
   # Make sure your network is whitelisted
@@ -33,8 +34,33 @@ def hello_world():
 class SimilarHeadRotation(Resource):
     def post(self):
         try:
-        # return {'inventory_no': result[1]}
-            return {'result': 'works'}
+            parser = reqparse.RequestParser()
+            parser.add_argument('pan', type=float, help='Left/Right rotation of head')
+            parser.add_argument('tilt', type=float, help='Up/Down rotation of head')
+            parser.add_argument('roll', type=float, help='Leaning of head')
+            args = parser.parse_args()
+
+            _pan = args['pan']
+            _tilt = args['tilt']
+            _roll = args['roll']
+
+            pan_tolerance = 3.0
+            tilt_tolerance = 3.0
+            roll_tolerance = 10.0
+
+            # query
+            cursor = db.cursor()
+            cursor.execute('SELECT * FROM zeitblick_db.Faces WHERE panAngle < %s AND panAngle > %s AND '
+                                                     'tiltAngle < %s AND tiltAngle > %s AND '
+                                                     'rollAngle < %s AND rollAngle > %s',
+                           (_pan + pan_tolerance, _pan - pan_tolerance,
+                            _tilt + tilt_tolerance, _tilt - tilt_tolerance,
+                            _roll + roll_tolerance, _roll - roll_tolerance,))
+
+            result = cursor.fetchone()
+
+            return {'inventory_no': result[1]}
+
         except Exception as e:
             return {'error': str(e)}
 
