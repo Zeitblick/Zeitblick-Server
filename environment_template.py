7e350a5b9da2f60279dfe7310767e97fd61ace82
bclausdorff <b.clausdorff@posteo.de>
2016-10-29 14:11:14 +0200

    Add template environment file

---
+++ environment_template.py
@@ -0,0 +1,10 @@
+# Rename this file into "environment.py" and fill in your own information
+env_config = dict(
+    MYSQL_USER='user',
+    MYSQL_PASSWORD='password',
+    MYSQL_DATABASE='database',
+    # This path was printed to the console when you started the proxy, and is of
+    # the format: `/[DIR]/[YOUR_PROJECT_ID]:[YOUR_REGION]:[YOUR_INSTANCE_NAME]`
+    MYSQL_SOCKET_PATH='socket_path',
+    MYSQL_IP='127.0.0.1'
+)
